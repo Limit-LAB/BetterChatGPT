5743d13895b13d9db278cd1414d6bbfe18503dad
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-06-19 23:48:38 +0800

    api

--- src/constants/auth.ts
+++ src/constants/auth.ts
@@ -1,7 +1,7 @@
-export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
-const customAPIEndpoint =
-  import.meta.env.VITE_CUSTOM_API_ENDPOINT || 'https://chatgpt-api.shn.hk/v1/';
-export const defaultAPIEndpoint =
-  import.meta.env.VITE_DEFAULT_API_ENDPOINT || officialAPIEndpoint;
+export const privateAPIEndpoint = 'https://api.saibo.ai/v1/intranet/openai/v1/chat/completions';
 
-export const availableEndpoints = [officialAPIEndpoint, customAPIEndpoint];
+export const officialAPIEndpoint = 'https://api.openai.com/v1/chat/completions';
+
+export const defaultAPIEndpoint = privateAPIEndpoint;
+
+export const availableEndpoints = [privateAPIEndpoint, officialAPIEndpoint];
